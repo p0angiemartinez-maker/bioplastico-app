78c78fc8f0cdba84f8dd01fd574b529f251bd095
p0angiemartinez-maker <p0.angiemartinez@gmail.com>
2025-11-17 20:09:07 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import BioplasticApp from "./BioplasticApp.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* Contenedor de pantalla completa y centrado */}
+    <div className="min-h-screen w-full bg-gray-50 py-8 px-4 flex justify-center">
+      <div className="w-full max-w-5xl">
+        <BioplasticApp />
+      </div>
+    </div>
   </React.StrictMode>
 );
