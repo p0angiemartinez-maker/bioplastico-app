9897980f673c4cccf028410571baf90467b3cc7d
p0angiemartinez-maker <p0.angiemartinez@gmail.com>
2025-11-17 19:04:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bioplastico-app/",
+  // rutas RELATIVAS, sirven bien en GitHub Pages
+  base: "./",
   plugins: [react()],
 });
